376252a90f5da80c10480f4b8b1bf250422c5aa5
brigettelundeen <brigette.lundeen@gmail.com>
2014-05-03 16:24:32 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('codefightsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
